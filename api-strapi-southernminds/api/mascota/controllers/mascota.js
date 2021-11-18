4cc9703830b327c6ef39917d6e1fd9a2e0200cf7
jonathanvolker <joni_451@hotmail.com>
2021-11-18 19:07:59 -0300

    stage back

--- api-strapi-southernminds/api/mascota/controllers/mascota.js
+++ api-strapi-southernminds/api/mascota/controllers/mascota.js
@@ -1,23 +1,53 @@
 'use strict';
-const {sanitizeEntity} = require('strapi-utils');
+const {sanitizeEntity, parseMultipartData,} = require('strapi-utils');
 
 
 
 module.exports = {
     async createAnimal(ctx) {
-        const {nombre, raza, sexo,edadAdulta,edadAprox, mes, aprox} = ctx.request.body;
-        try {
-            var aproximada = false;
-          
-                const meses = parseInt(mes)
-                if(aprox === "true"){
-                    aproximada = true
-                } 
-             console.log(strapi.services)
-            console.log(ctx.request.body)
-           const animal= await strapi.services.mascota.create({nombre,raza, sexo,edadAdulta, edadAprox, meses, aproximada})
+        const {nombre, raza, sexo,fechaNacimiento,edadAprox, meses, aprox} = ctx.request.body;
+        const date = new Date();
+        const obj ={
+            nombre,
+            raza,
+            sexo,
+            fechaNacimiento,
+            aproximada,
+            meses
+
+        };
+        var aproximada = false;
+      
+        if(aprox === "true"){
+                aproximada = true
+               // console.log(ctx.request.body)
+                const aprox=parseInt(edadAprox)
+               
+                    const newDate = date.getFullYear()-aprox + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+                    obj.fechaNacimiento = newDate;
+        } 
+        if(meses > 0){
+            const newDate = date.getFullYear() + "-" + (date.getMonth() + 1 - meses) + "-" + date.getDate();
+            obj.fechaNacimiento = newDate;
         
-            ctx.send(animal)
+        }
+        
+        obj.aproximada = aproximada;
+        console.log(obj)
+     
+
+        try {
+          
+
+            let entity;
+            if (ctx.is('mascota')) {
+              const { data, files } = parseMultipartData(ctx);
+              
+              entity = await strapi.services.mascota.create(data, { files });
+            } else {
+              entity = await strapi.services.mascota.create(obj);
+            }
+            return sanitizeEntity(entity, { model: strapi.models.mascota });
         
       } catch (error) {
          ctx.send(error) 
